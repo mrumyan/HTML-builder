e9a70761cc79cd7d53b7ecb6fbb7c95a22f2293a
Maria Rumyantseva <manechka96@list.ru>
2023-05-07 16:57:47 +0300

    feat: add 1st task solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(filePath, 'utf-8');
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));
