bb21225433bb41a551e4b7ae52ba28bdc2856a25
Maria Rumyantseva <manechka96@list.ru>
2023-05-09 02:04:55 +0300

    feat: add 4th task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirPath = path.join(__dirname, 'files');
+const newDirPath = path.join(__dirname, 'files-copy');
+
+const copyFile = (fileName) => {
+    fs.copyFile(path.join(dirPath, fileName), path.join(newDirPath, fileName), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+};
+
+fs.mkdir(newDirPath, { recursive: true }, error => {
+    if (!error) {
+        fs.readdir(dirPath, { withFileTypes: true }, (error, files) => {
+            if (!error) {
+                for (let file of files) {
+                    if (file.isFile()) {
+                        copyFile(file.name);
+                    }
+                }
+            } else {
+                console.log(error);
+            }
+        });
+    } else {
+        console.log(error);
+    }
+});
